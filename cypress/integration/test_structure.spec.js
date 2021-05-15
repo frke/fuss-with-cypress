2118bd3f3313e010fbbac1441d374b41ca1f4ba5
milanranisavljevic <milanranisavljevic88@gmail.com>
2021-05-15 04:19:46 +0200

    Add TEST STRUCTURE example

---
+++ cypress/integration/test_structure.spec.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+context('SUITE', () => {
+
+	beforeEach(() => {
+		cy.log("CONTEXT LEVEL")
+	})
+
+	describe('GROUP I', () => {
+
+		it('Verify nested 1', () => {
+			cy.log("GROUP Test")
+		});
+
+		it('Verify nested 2', () => {
+			cy.log("GROUP Test")
+		});
+
+		it('Verify nested 3', () => {
+			cy.log("GROUP Test")
+		});
+	})
+
+	describe('GROUP II', () => {
+
+		beforeEach(() => {
+			cy.log("GROUP LEVEL")
+		})
+
+		it('Verify nested 1', () => {
+			cy.log("GROUP Test")
+		});
+
+		it('Verify nested 2', () => {
+			cy.log("GROUP Test")
+		});
+	})
+
+	it('Verify 1', () => {
+		cy.log("Test")
+	})
+
+	it('Verify 2', () => {
+		cy.log("Test")
+	})
+
+})
